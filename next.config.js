8813e98255bec83d0c04306c2af0a739a23a8bdf
Yash Patil <yashpatil822003@gmail.com>
2025-11-03 13:29:51 +0530

    Remove production-only export config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  ...(isProduction && { output: 'export' }),
   images: {
     unoptimized: true,
   },
